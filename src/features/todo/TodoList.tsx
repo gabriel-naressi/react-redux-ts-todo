99884445a5e2e85cde94b45f5f535eb4c994eb8f
Gabriel Naressi <gabrielnaressidefabris@gmail.com>
2021-01-06 22:45:38 -0300

    Add loading on fetch data

--- src/features/todo/TodoList.tsx
+++ src/features/todo/TodoList.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import TodoItem from './TodoItem'
 import { selectTodos } from '../../features/todo/todoSlice'
+import { RootState } from '../../app/store';
 
 const TodoList = () => {
+  const { status } = useSelector((state: RootState) => state.todo);
   const todos = useSelector(selectTodos);
+
   return (
-    <ul>
-      {todos.map(todo => <TodoItem todo={todo} key={todo.id} />)}
-    </ul>
+    <>
+      {status !== 'idle' ? <p>Loading...</p> : <></>}
+      <ul>
+        {todos.map(todo => <TodoItem todo={todo} key={todo.id} />)}
+      </ul>
+    </>
   );
 
 };
