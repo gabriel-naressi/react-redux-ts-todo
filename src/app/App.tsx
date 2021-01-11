d4a07f73a8a1764cc0f478a58faac3c349e14b33
Gabriel <gabriel.fabris@synchro.com.br>
2021-01-11 15:16:10 -0300

    Move app.tsx to /app directory and create common empty directory

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
-import routes from './routes'
+import routes from '../routes'
 
 const App = () => (
   <Router>
