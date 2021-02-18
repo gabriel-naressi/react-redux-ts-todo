27a18223190550d3cd19004d5ce81c6dad5c8013
Gabriel Naressi <gabrielnaressidefabris@gmail.com>
2021-02-17 21:28:26 -0300

    Remove função connect do counter.tsx

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,31 +1,19 @@
 import React from 'react'
-import { connect, ConnectedProps, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
+import { useAppDispatch } from '../../app/store'
 import { increment, selectCount } from '../counter/counterSlice'
 
-const mapDispatch = { increment }
-
-const connector = connect(
-  null,
-  mapDispatch
-)
-
-type PropsFromRedux = ConnectedProps<typeof connector>
-interface CounterProps extends PropsFromRedux {}
-
-const Counter = ({ increment } : CounterProps) => {
+const Counter = () => {
+  const dispatch = useAppDispatch()
   const count = useSelector(selectCount)
-  //const dispatch = useDispatch()
 
   return (
     <div>
       <p>Counter page</p>
-      {/*<button style={{marginRight: '5px'}} onClick={() => dispatch(increment())}>+</button>*/}
-      <button style={{marginRight: '5px'}} onClick={() => increment()}>+</button>
+      <button style={{marginRight: '5px'}} onClick={() => dispatch(increment())}>+</button>
       Clicked {count} times
     </div>
   )
 }
 
-
-
-export default connector(Counter)
+export default Counter
