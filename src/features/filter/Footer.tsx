6c8b2d8f50c1a5bda88ab9e0301772c7e2a25181
Gabriel Naressi <gabrielnaressidefabris@gmail.com>
2021-02-17 22:02:02 -0300

    Code style

--- src/features/filter/Footer.tsx
+++ src/features/filter/Footer.tsx
@@ -11,21 +11,21 @@ const Footer = () => {
     <div>
       <span>Show: </span>
       <button
-        style={{ marginLeft: '4px'}}
+        style={{ marginLeft: '4px' }}
         onClick={() => dispatch(setFilter(VisibilityFilters.SHOW_ALL))}
         disabled={active === VisibilityFilters.SHOW_ALL}
       >
         All
       </button>
       <button
-        style={{ marginLeft: '4px'}}
+        style={{ marginLeft: '4px' }}
         onClick={() => dispatch(setFilter(VisibilityFilters.SHOW_ACTIVE))}
         disabled={active === VisibilityFilters.SHOW_ACTIVE}
       >
         Active
       </button>
       <button
-        style={{marginLeft: '4px'}}
+        style={{marginLeft: '4px' }}
         onClick={() => dispatch(setFilter(VisibilityFilters.SHOW_COMPLETED))}
         disabled={active === VisibilityFilters.SHOW_COMPLETED}
       >
